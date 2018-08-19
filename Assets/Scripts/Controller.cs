e2d860c1227a109d3e0f21d800704e1245fcfc74
רועי מדמון <roipk123@gmail.com>
2018-08-19 17:17:38 +0300

    clear tunnel

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -19,26 +19,6 @@ public class Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        /*  if (x != 0 && y != 0)
-          float x = CrossPlatformInputManager.GetAxis("Horizontal");
-          float y = CrossPlatformInputManager.GetAxis("Vertical");
-          Vector3 movment = new Vector3(x, 0, y);
-          rb.velocity = movment * 4f;
-
-          {
-              transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
-          }
-
-              if (x != 0 || y != 0)
-          {
-              anim.Play("Walk");
-          }
-
-          else
-          {
-              anim.Play("Idle");
-          }
-         */
 
         
 
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+    public GameObject computer;
+    public GameObject enemy;
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = computer.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = enemy.transform.position - computer.transform.position;
+
+         if (direction.magnitude < 10f)
+         {
+            anim.Play("WAIT04");
+         }
+         else
+            anim.Play("WAIT02");
+    }
+}
