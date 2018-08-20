bda61f222a5a6ac99f90cb5d0bca8fb0c5370927
רועי מדמון <roipk123@gmail.com>
2018-08-20 12:09:23 +0300

    update

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Walk : MonoBehaviour {
+    protected JoyButton joyButton;
 
     Animation anim;
     Rigidbody rg;
@@ -12,10 +13,13 @@ public class Walk : MonoBehaviour {
     float speedWalk = 200f;
     float speedRound;
     bool turned = false;
+    bool block = false;
+
     // Use this for initialization
     void Start () {
         anim = player.GetComponent<Animation>();
         rg = player.GetComponent<Rigidbody>();
+        joyButton = FindObjectOfType<JoyButton>();
         speedRound = speedWalk / 2;
     }
 	
@@ -23,15 +27,43 @@ public class Walk : MonoBehaviour {
 	void Update () {
         float x = CrossPlatformInputManager.GetAxis("Horizontal");
         float y = CrossPlatformInputManager.GetAxis("Vertical");
-
+        
         Vector3 direction = enemy.transform.position - player.transform.position;
+        //Debug.Log(block);
+        if(!block && joyButton.Pressed)
+        {
+            block = true;
+            
+            anim.Play("Skill");
+        }
 
-        if (direction.magnitude < 10f)
+        if (block && joyButton.Pressed)
+        {
+
+            Debug.Log(anim.IsPlaying("Skill"));
+
+
+            if (!anim.IsPlaying("Skill"))
+                anim.Play("Idle");
+           
+            
+        }
+
+        if (block && !joyButton.Pressed)
+        {
+           // Debug.Log("test 2");
+            block = false;
+            //anim.Play("Block");
+        }
+
+      
+
+        if (direction.magnitude < 10f && !block)
         {
             anim.Play("Attack");
         }
 
-        if (x != 0 && y != 0)
+        if (x != 0 && y != 0 && !block)
         {
             // turned = check(y, turned);
             player.transform.Rotate(Vector3.up, x* speedRound * Time.deltaTime);
@@ -41,12 +73,12 @@ public class Walk : MonoBehaviour {
 
         if (x!=0 || y != 0)
         {
-            if (!anim.IsPlaying("Attack"))
+            if (!anim.IsPlaying("Attack") && !block)
                 anim.Play("Walk");
         }
         else
         {
-            if(!anim.IsPlaying("Attack"))
+            if(!anim.IsPlaying("Attack") && !block)
                 anim.Play("Idle");
             player.transform.Rotate(Vector3.up,0f);
             rg.velocity = (player.transform.forward * 0f);
